d62b17f018e0d346615506f4486141db2aa2bf12
toscy <123191534+Lajous-Harold@users.noreply.github.com>
2024-12-13 12:24:04 +0100

    Activity 6
    - rename invoke to generatePrimes
    - extract printNumbers method into method object named NumberPrinter
    - Move the new inner class NumberPrinter to top level
    - do not pass outer class in new class constructor
    - ignore the missing variables warning and continue
    - add int before pagenumber and pageoffset
    - add int before rowoffset and column
    - pass int linesPerPage, int columns in constructor, make them fields
    - pass the values in the call to printNumbers

--- start/cleancode-kata-primeprinter/src/main/java/com/cleancode/knuth/PrimePrinter.java
+++ start/cleancode-kata-primeprinter/src/main/java/com/cleancode/knuth/PrimePrinter.java
@@ -1,11 +1,14 @@
 package com.cleancode.knuth;
 
 public class PrimePrinter {
+
     private static final int numberOfNumbers = 1000;
 
     public static void main(String[] args) {
         PrimePrinterHelper helper = new PrimePrinterHelper();
-        int[] primes = helper.generateNumbers(numberOfNumbers);
-        helper.printNumbers(primes, numberOfNumbers);
+        int[] primes = helper.generatePrimes(numberOfNumbers);
+
+        NumberPrinter printer = new NumberPrinter(50, 4);
+        printer.printNumbers(primes, numberOfNumbers);
     }
 }
--- start/cleancode-kata-primeprinter/src/main/java/com/cleancode/knuth/PrimePrinterHelper.java
+++ start/cleancode-kata-primeprinter/src/main/java/com/cleancode/knuth/PrimePrinterHelper.java
@@ -2,14 +2,8 @@ package com.cleancode.knuth;
 
 public class PrimePrinterHelper {
 
-    private final int linesPerPage = 50;
-    private final int columns = 4;
     private final int ordmax = 30;
     private int numbers[];
-    private int pagenumber;
-    private int pageoffset;
-    private int rowoffset;
-    private int column;
     private int candidate;
     private int primeIndex;
     private boolean possiblyPrime;
@@ -18,7 +12,7 @@ public class PrimePrinterHelper {
     private int n = 0;
     private int multiples[] = new int[ordmax + 1];
 
-    public int[] generateNumbers(int numberOfNumbers) {
+    public int[] generatePrimes(int numberOfNumbers) {
 
         numbers = new int[numberOfNumbers + 1];
         candidate = 1;
@@ -51,18 +45,28 @@ public class PrimePrinterHelper {
 
         return numbers;
     }
+}
+
+class NumberPrinter {
+    private final int linesPerPage;
+    private final int columns;
+
+    public NumberPrinter(int linesPerPage, int columns) {
+        this.linesPerPage = linesPerPage;
+        this.columns = columns;
+    }
 
     public void printNumbers(int[] numbers, int numberOfNumbers) {
-        pagenumber = 1;
-        pageoffset = 1;
+        int pagenumber = 1;
+        int pageoffset = 1;
         while (pageoffset <= numberOfNumbers) {
             System.out.print("The First ");
             System.out.print(Integer.toString(numberOfNumbers));
             System.out.print(" Prime Numbers === Page ");
             System.out.print(Integer.toString(pagenumber));
             System.out.println("\n");
-            for (rowoffset = pageoffset; rowoffset <= pageoffset + linesPerPage - 1; rowoffset++) {
-                for (column = 0; column <= columns - 1; column++)
+            for (int rowoffset = pageoffset; rowoffset <= pageoffset + linesPerPage - 1; rowoffset++) {
+                for (int column = 0; column <= columns - 1; column++)
                     if (rowoffset + column * linesPerPage <= numberOfNumbers)
                         System.out.printf("%10d", numbers[rowoffset + column * linesPerPage]);
                 System.out.println();
